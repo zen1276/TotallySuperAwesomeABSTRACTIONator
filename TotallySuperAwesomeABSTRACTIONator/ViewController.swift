a79abcf39933efe9c305fd9d36efe15af0ad1c44
Van hoffman, Zendan <zvan1276@740s-pl-119.local>
2018-12-04 08:27:01 -0700

    Initial Commit

---
+++ TotallySuperAwesomeABSTRACTIONator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TotallySuperAwesomeABSTRACTIONator
+//
+//  Created by Van hoffman, Zendan on 12/4/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
